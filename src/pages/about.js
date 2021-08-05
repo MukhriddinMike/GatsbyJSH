fd6cdf23e6bbb1d411bd22f3eece4f9e0f139d0a
MukhriddinMike <miwady@mail.ru>
2021-08-06 01:39:06 +0900

    finished at 1

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+const about = () => {
+  return (
+    <div>
+      <h1>hello from about page</h1>
+      <div>
+        <Link to="/"> Back</Link>
+      </div>
+    </div>
+  )
+}
+
+export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
-
+import { Link } from "gatsby"
+import Navbar from "../components/Navbar"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello Mike!</h1>
+      <div>
+        <Link to="/about">About</Link>
+      </div>
+      <div>
+        <Link to="/company/history">History</Link>
+      </div>
+      <div>
+        <Link to="/testing">Testing</Link>
+      </div>
+    </div>
+  )
 }
